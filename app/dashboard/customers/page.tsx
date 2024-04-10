49294f08152376dfcb5a2e51d7d9c358d8c0ad1e
Siddhartha Singh <falsigo@hotmail.com>
2024-04-10 14:41:39 +0530

    deploy

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -12,16 +12,15 @@ export default function Page() {
         'https://archive.pib.gov.in/apipib/api/Getsearch',
         {
           method: 'POST',
-          mode: 'no-cors',
           headers: {
             'Content-Type': 'application/json',
             Authorization:
               'Bearer 15dpV1NVTyHXVmZeo3uFSaA13570Qu9mpUj5N6SxAGTiGBE78Lw9XKfyNSwh1JFeYdeZh-ywsQsfmxK7FIynzp5bL13ug8rP4GydsOQgs-a-iMrYNuCYQkpoVof_FP2QFypnflJX9avcbS-AWegigLhXVwDErejKN6tctWjsD2omBF9JzocUMT5biA3YutTUdcflW9wYWYfBzlOV5erA1Lks6YlovOYkGRdQJzHHlpB5mGoXXE6Mr2fTW-93AOmeKsfEUWL29ziY7Yy30h37oCL-UAtMNOgr5NOGQ2A28mM',
           },
           body: JSON.stringify({
-            langid: languageId,
-            startDate: startDate,
-            endDate: endDate,
+            langid: 1,
+            startDate: '01-04-2024',
+            endDate: '10-04-2024',
           }),
         },
       );
@@ -40,7 +39,7 @@ export default function Page() {
   };
   useEffect(() => {
     fetchData(); // Fetch data when component mounts
-  }, []); // Empty dependency array ensures this effect runs only once
+  }); // Empty dependency array ensures this effect runs only once
 
   return <p>Customers Page</p>;
 }
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,21 +7,44 @@ import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({ invalid_type_error: 'Please select a number' }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status',
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
-
-export async function createInvoice(formdata: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+/* prevState - contains the state passed from the useFormState hook. You won't be using it in the action in this example, but it's a required prop. */
+export async function createInvoice(prevState: State, formdata: FormData) {
+  //validate form using Zod
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formdata.get('customerId'),
     amount: formdata.get('amount'),
     status: formdata.get('status'),
   });
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+  const { customerId, amount, status } = validatedFields.data;
+
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
@@ -37,13 +60,24 @@ export async function createInvoice(formdata: FormData) {
   redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formdata: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formdata: FormData,
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formdata.get('customerId'),
     amount: formdata.get('amount'),
     status: formdata.get('status'),
   });
 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
 
   try {
@@ -61,7 +95,7 @@ export async function updateInvoice(id: string, formdata: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
+  // throw new Error('Failed to Delete Invoice');
 
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
